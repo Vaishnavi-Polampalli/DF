3cd57afa17ba75fef580f6497937ebfb00127577
Vaishnavi Polampalli <polampvi@mail.uc.edu>
2024-08-06 02:00:39 -0400

    Initial commit

---
+++ spring/src/main/java/com/df/spring/BuildProjectApplication.java
@@ -0,0 +1,14 @@
+package com.df.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildProjectApplication.class, args);
+
+	}
+
+}
